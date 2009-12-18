a8bd410121bdbf2c354ca95bcc5d3cb61e7595cb
Malcolm.Wallace <Malcolm.Wallace@cs.york.ac.uk>
2009-12-18 14:02:49 +0000

    Derive Data.Accessor.Accessor for AreaSpots.

--- Graphics/Rendering/Chart/AreaSpots.hs
+++ Graphics/Rendering/Chart/AreaSpots.hs
@@ -1,3 +1,14 @@
+-- |
+-- Module     :  Graphics.Rendering.Chart.AreaSpots
+-- Copyright  :  (c) Malcolm Wallace 2009
+-- License    :  BSD-style (see COPYRIGHT file)
+--
+-- Area spots are a collection of unconnected filled circles,
+-- with x,y position, and an independent z value to be represented
+-- by the relative area of the spots.
+
+{-# OPTIONS_GHC -XTemplateHaskell #-}
+
 module Graphics.Rendering.Chart.AreaSpots
   ( AreaSpots(..)
   , defaultAreaSpots
@@ -8,8 +19,8 @@ import qualified Graphics.Rendering.Cairo as C
 import Graphics.Rendering.Chart.Types
 import Graphics.Rendering.Chart.Plot
 import Graphics.Rendering.Chart.Axis
+import Data.Accessor.Template
 import Data.Colour
-import Data.Colour.SRGB (sRGB) -- ???
 import Data.Colour.Names
 
 import Control.Monad
@@ -23,44 +34,44 @@ thd3 (_,_,a) = a
 -- | A collection of unconnected spots, with x,y position, and an
 --   independent z value to be represented by the area of the spot.
 data AreaSpots z x y = AreaSpots
-  { area_spots_title      :: String
-  , area_spots_linethick  :: Double
-  , area_spots_linecolour :: AlphaColour Double
-  , area_spots_fillcolour :: AlphaColour Double
-  , area_spots_max_radius :: Double	-- ^ the largest size of spot
-  , area_spots_values     :: [(x,y,z)]
+  { area_spots_title_      :: String
+  , area_spots_linethick_  :: Double
+  , area_spots_linecolour_ :: AlphaColour Double
+  , area_spots_fillcolour_ :: AlphaColour Double
+  , area_spots_max_radius_ :: Double	-- ^ the largest size of spot
+  , area_spots_values_     :: [(x,y,z)]
   }
 
 defaultAreaSpots :: AreaSpots z x y
 defaultAreaSpots = AreaSpots
-  { area_spots_title      = ""
-  , area_spots_linethick  = 0.1
-  , area_spots_linecolour = opaque blue
-  , area_spots_fillcolour = flip withOpacity 0.2 blue
-  , area_spots_max_radius = 20  -- in pixels
-  , area_spots_values     = []
+  { area_spots_title_      = ""
+  , area_spots_linethick_  = 0.1
+  , area_spots_linecolour_ = opaque blue
+  , area_spots_fillcolour_ = flip withOpacity 0.2 blue
+  , area_spots_max_radius_ = 20  -- in pixels
+  , area_spots_values_     = []
   }
 
 instance (PlotValue z) => ToPlot (AreaSpots z) where
     toPlot p = Plot { plot_render_ = renderAreaSpots p
-                    , plot_legend_ = [(area_spots_title p, renderSpotLegend p)]
-                    , plot_all_points_ = ( map fst3 (area_spots_values p)
-                                         , map snd3 (area_spots_values p) )
+                    , plot_legend_ = [(area_spots_title_ p, renderSpotLegend p)]
+                    , plot_all_points_ = ( map fst3 (area_spots_values_ p)
+                                         , map snd3 (area_spots_values_ p) )
                     }
 
 renderAreaSpots  :: (PlotValue z) =>
                     AreaSpots z x y -> PointMapFn x y -> CRender ()
 renderAreaSpots p pmap = preserveCState $
-    forM_ (scaleMax ((area_spots_max_radius p)^2)
-                    (area_spots_values p))
+    forM_ (scaleMax ((area_spots_max_radius_ p)^2)
+                    (area_spots_values_ p))
           (\ (x,y,z)-> do
               let radius = sqrt z
               let (CairoPointStyle drawSpotAt)    = filledCircles radius
-                                                       (area_spots_fillcolour p)
+                                                      (area_spots_fillcolour_ p)
               drawSpotAt (pmap (LValue x, LValue y))
               let (CairoPointStyle drawOutlineAt) = hollowCircles radius
-                                                       (area_spots_linethick p)
-                                                       (area_spots_linecolour p)
+                                                      (area_spots_linethick_ p)
+                                                      (area_spots_linecolour_ p)
               drawOutlineAt (pmap (LValue x, LValue y))
           )
   where
@@ -74,13 +85,16 @@ renderSpotLegend p r@(Rect p1 p2) = preserveCState $ do
     let radius = min (abs (p_y p1 - p_y p2)) (abs (p_x p1 - p_x p2))
         centre = linearInterpolate p1 p2
     let (CairoPointStyle drawSpotAt)    = filledCircles radius
-                                            (area_spots_fillcolour p)
+                                            (area_spots_fillcolour_ p)
     drawSpotAt centre
     let (CairoPointStyle drawOutlineAt) = hollowCircles radius
-                                            (area_spots_linethick p)
-                                            (area_spots_linecolour p)
+                                            (area_spots_linethick_ p)
+                                            (area_spots_linecolour_ p)
     drawOutlineAt centre
   where
     linearInterpolate (Point x0 y0) (Point x1 y1) =
         Point (x0 + abs(x1-x0)/2) (y0 + abs(y1-y0)/2)
 
+-------------------------------------------------------------------------
+-- Template haskell to derive Data.Accessor.Accessor
+$( deriveAccessors ''AreaSpots )
