6f4f4f6b16f0ea2f9f2d6a294c3633714d7c4846
Jan Bracker <jan.bracker@googlemail.com>
2013-06-09 20:25:10 +0200

    Added a module for the future cairo backend with some smaller utilities.

---
+++ chart/Graphics/Rendering/Chart/Backend/Cairo.hs
@@ -0,0 +1,25 @@
+
+-- | The backend to render charts with cairo.
+module Graphics.Rendering.Chart.Backend.Cairo
+  ( convertLineCap, convertLineJoin
+  ) where
+
+
+import qualified Graphics.Rendering.Cairo as C
+
+import qualified Graphics.Rendering.Chart.Drawing as G
+
+-- | Convert a charts line join to a cairo line join.
+convertLineJoin :: G.LineJoin -> C.LineJoin
+convertLineJoin lj = case lj of
+  G.LineJoinMiter -> C.LineJoinMiter
+  G.LineJoinRound -> C.LineJoinRound
+  G.LineJoinBevel -> C.LineJoinBevel
+
+-- | Convert a charts line cap to a cairo line cap.
+convertLineCap :: G.LineCap -> C.LineCap
+convertLineCap lc = case lc of
+  G.LineCapRound  -> C.LineCapRound
+  G.LineCapButt   -> C.LineCapButt
+  G.LineCapSquare -> C.LineCapSquare
+
