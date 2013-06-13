aa818a3d1782d6474e12f0731280e9106b931026
Jan Bracker <jan.bracker@googlemail.com>
2013-06-13 14:07:52 +0200

    Added Matrix API from Cairo.

--- chart/Graphics/Rendering/Chart/Geometry.hs
+++ chart/Graphics/Rendering/Chart/Geometry.hs
@@ -1,24 +1,29 @@
-module Graphics.Rendering.Chart.Geometry(
-    Rect(..),
-    Point(..),
-    Vector(..),
+module Graphics.Rendering.Chart.Geometry
+  ( Rect(..)
+  , Point(..)
+  , Vector(..)
 
-    RectSize,
-    Range,
+  , RectSize
+  , Range
 
-    mkrect,
-    rectPath,
-    pvadd,
-    pvsub,
-    psub,
-    vscale,
-    within,
+  , mkrect
+  , rectPath
+  , pvadd
+  , pvsub
+  , psub
+  , vscale
+  , within
 
-    RectEdge(..),
-    Limit(..),
-    PointMapFn,
-
-    ) where
+  , RectEdge(..)
+  , Limit(..)
+  , PointMapFn
+    
+  , Matrix
+  , identity
+  , rotate
+  , scale
+  , translate
+  ) where
 
 -- | A point in two dimensions.
 data Point = Point {
@@ -78,4 +83,75 @@ rectPath (Rect p1@(Point x1 y1) p3@(Point x2 y2)) = [p1,p2,p3,p4,p1]
   where    
     p2 = (Point x1 y2)
     p4 = (Point x2 y1)
+
+-- -----------------------------------------------------------------------
+-- Matrix Type
+-- -----------------------------------------------------------------------
+
+-- | Copied from Graphics.Rendering.Cairo.Matrix
+data Matrix = Matrix { xx :: !Double, yx :: !Double,
+                       xy :: !Double, yy :: !Double,
+                       x0 :: !Double, y0 :: !Double }
+
+-- | Copied from Graphics.Rendering.Cairo.Matrix
+instance Num Matrix where
+  (*) (Matrix xx yx xy yy x0 y0) (Matrix xx' yx' xy' yy' x0' y0') =
+    Matrix (xx * xx' + yx * xy')
+           (xx * yx' + yx * yy')
+           (xy * xx' + yy * xy')
+           (xy * yx' + yy * yy')
+           (x0 * xx' + y0 * xy' + x0')
+           (x0 * yx' + y0 * yy' + y0')
+
+  (+) = pointwise2 (+)
+  (-) = pointwise2 (-)
+
+  negate = pointwise negate
+  abs    = pointwise abs
+  signum = pointwise signum
+  
+  fromInteger n = Matrix (fromInteger n) 0 0 (fromInteger n) 0 0
+
+-- | Copied from Graphics.Rendering.Cairo.Matrix
+{-# INLINE pointwise #-}
+pointwise f (Matrix xx yx xy yy x0 y0) =
+  Matrix (f xx) (f yx) (f xy) (f yy) (f x0) (f y0)
+
+-- | Copied from Graphics.Rendering.Cairo.Matrix
+{-# INLINE pointwise2 #-}
+pointwise2 f (Matrix xx yx xy yy x0 y0) (Matrix xx' yx' xy' yy' x0' y0') =
+  Matrix (f xx xx') (f yx yx') (f xy xy') (f yy yy') (f x0 x0') (f y0 y0')
+
+-- | Copied from Graphics.Rendering.Cairo.Matrix
+identity :: Matrix
+identity = Matrix 1 0 0 1 0 0
+
+-- | Copied and adopted from Graphics.Rendering.Cairo.Matrix
+translate :: Vector -> Matrix -> Matrix
+translate tv m = m * (Matrix 1 0 0 1 (t_x tv) (t_y tv))
+
+-- | Copied and adopted from Graphics.Rendering.Cairo.Matrix
+scale :: Vector -> Matrix -> Matrix
+scale sv m = m * (Matrix (v_x sv) 0 0 (v_y sv) 0 0)
+
+-- | Copied from Graphics.Rendering.Cairo.Matrix
+rotate :: Double -> Matrix -> Matrix
+rotate r m = m * (Matrix c s (-s) c 0 0)
+  where s = sin r
+        c = cos r
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
