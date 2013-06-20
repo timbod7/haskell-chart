e3f94c4713499ea5d36cf721c9facfd526447761
Jan Bracker <jan.bracker@googlemail.com>
2013-06-20 17:05:33 +0200

    Documentation for the backend API.

--- chart/Graphics/Rendering/Chart/Backend.hs
+++ chart/Graphics/Rendering/Chart/Backend.hs
@@ -1,6 +1,7 @@
 
 {-# LANGUAGE FlexibleContexts #-}
 
+-- | This module provides the interface class for 'ChartBackend's.
 module Graphics.Rendering.Chart.Backend
   ( ChartBackendEnv(..)
   , defaultEnv
@@ -29,29 +30,42 @@ import Graphics.Rendering.Chart.Geometry
 -- Rendering Backend Environment
 -- -----------------------------------------------------------------------
 
--- | The environment present in the CRender Monad.
+-- | The environment present in the 'ChartBackend'.
 data ChartBackendEnv = ChartBackendEnv
-  -- | An adjustment applied immediately prior to points
+  { cbePointAlignFn :: Point -> Point
+  -- ^ An adjustment applied immediately prior to points
   --   being displayed in device coordinates.
   --
   --   When device coordinates correspond to pixels, a cleaner
   --   image is created if this transform rounds to the nearest
   --   pixel. With higher-resolution output, this transform can
   --   just be the identity function.
-  { cbePointAlignFn :: Point -> Point
 
   -- | A adjustment applied immediately prior to coordinates
   --   being transformed.
   , cbeCoordAlignFn :: Point -> Point
   
+  -- | The current transformation.
   , cbeTransform :: Matrix
+  
+  -- | The current 'FontStyle'.
   , cbeFontStyle :: FontStyle
+  
+  -- | The current 'FillStyle'.
   , cbeFillStyle :: FillStyle
+  
+  -- | The current 'LineStyle'.
   , cbeLineStyle :: LineStyle
+  
+  -- | The current clip region, if there is one.
   , cbeClipRegion :: Maybe Rect
   }
 
-defaultEnv :: (Point -> Point) -> (Point -> Point) -> ChartBackendEnv
+-- | Produce a environment with no transformation and clipping. 
+--   It will use the default styles.
+defaultEnv :: (Point -> Point) -- ^ The point alignment function ('cbePointAlignFn')
+           -> (Point -> Point) -- ^ The coordinate alignment function ('cbeCoordAlignFn')
+           -> ChartBackendEnv
 defaultEnv pointAlignFn coordAlignFn = ChartBackendEnv 
   { cbePointAlignFn = pointAlignFn
   , cbeCoordAlignFn = coordAlignFn
@@ -66,49 +80,70 @@ defaultEnv pointAlignFn coordAlignFn = ChartBackendEnv
 -- Rendering Backend Class
 -- -----------------------------------------------------------------------
 
+-- | A 'ChartBackend' provides the capability to render a chart somewhere.
+--   
+--   You have to implement all functions of the interface.
 class (Monad m, MonadReader ChartBackendEnv m) => ChartBackend m where
   
   -- | Stroke the outline of the given path using the 
-  --   current line style. This function does /not/ perform
+  --   current 'LineStyle'. This function does /not/ perform
   --   alignment operations on the path.
   strokePath :: Path -> m ()
   
-  -- | Fill the given path using the current fill style.
+  -- | Fill the given path using the current 'FillStyle'.
   --   The given path will be closed prior to filling.
   --   This function does /not/ perform
   --   alignment operations on the path.
   fillPath :: Path -> m ()
   
-  -- | Fill the clip region using the current fill style.
+  -- | Fill the clip region using the current 'FillStyle'.
   fillClip :: m ()
   
   -- | Calculate a 'TextSize' object with rendering information
-  --   about the given string.
+  --   about the given string without actually rendering it.
   textSize :: String -> m TextSize
   
   -- | Draw a single-line textual label anchored by the baseline (vertical) 
-  --   left (horizontal) point. Uses the current 'FontStyle'.
+  --   left (horizontal) point. Uses the current 'FontStyle' for drawing.
   drawText :: Point -> String -> m ()
   
   -- | Apply the given transformation in this local
   --   environment when drawing. The given transformation 
-  --   is applied to the current transformation.
+  --   is applied after the current transformation. This
+  --   means both are combined.
+  --   
+  --   Use the 'withTransform'' function to correctly update
+  --   your environment when implementing this function.
   withTransform :: Matrix -> m a -> m a
   
   -- | Use the given font style in this local
   --   environment when drawing text.
+  --   
+  --   Use the 'withFontStyle'' function to correctly update
+  --   your environment when implementing this function.
   withFontStyle :: FontStyle -> m a -> m a
   
   -- | Use the given fill style in this local
   --   environment when filling paths.
+  --   
+  --   Use the 'withFillStyle'' function to correctly update
+  --   your environment when implementing this function.
   withFillStyle :: FillStyle -> m a -> m a
   
   -- | Use the given line style in this local
   --   environment when stroking paths.
+  --   
+  --   Use the 'withLineStyle'' function to correctly update
+  --   your environment when implementing this function.
   withLineStyle :: LineStyle -> m a -> m a
   
   -- | Use the given clipping rectangle when drawing
-  --   in this local environment.
+  --   in this local environment. The new clipping region
+  --   is intersected with the given clip region. You cannot 
+  --   escape the clip!
+  --   
+  --   Use the 'withClipRegion'' function to correctly update
+  --   your environment when implementing this function.
   withClipRegion :: Rect -> m a -> m a
 
 -- -----------------------------------------------------------------------
@@ -128,33 +163,50 @@ data TextSize = TextSize
 -- Rendering Utility Functions
 -- -----------------------------------------------------------------------
 
+-- | Get the current transformation.
 getTransform :: ChartBackend m => m Matrix
 getTransform = liftM cbeTransform ask
 
+-- | Get the current font style.
 getFontStyle :: ChartBackend m => m FontStyle
 getFontStyle = liftM cbeFontStyle ask
 
+-- | Get the current fill style.
 getFillStyle :: ChartBackend m => m FillStyle
 getFillStyle = liftM cbeFillStyle ask
 
+-- | Get the current line style.
 getLineStyle :: ChartBackend m => m LineStyle
 getLineStyle = liftM cbeLineStyle ask
 
+-- | Get the current clipping region.
+--   If no clipping region was set (it is an infinite plane) 
+--   'Nothing' is returned.
 getClipRegion :: ChartBackend m => m (Maybe Rect)
 getClipRegion = liftM cbeClipRegion ask
 
+-- | Helper to correctly update the backends environment when 
+--   implementing 'withTransform'.
 withTransform' :: ChartBackend m => Matrix -> m a -> m a
 withTransform' t m = local (\s -> s { cbeTransform = (cbeTransform s) * t }) m
 
+-- | Helper to correctly update the backends environment when 
+--   implementing 'withFontStyle'.
 withFontStyle' :: ChartBackend m => FontStyle -> m a -> m a
 withFontStyle' fs m = local (\s -> s { cbeFontStyle = fs }) m
 
+-- | Helper to correctly update the backends environment when 
+--   implementing 'withFillStyle'.
 withFillStyle' :: ChartBackend m => FillStyle -> m a -> m a
 withFillStyle' fs m = local (\s -> s { cbeFillStyle = fs }) m
 
+-- | Helper to correctly update the backends environment when 
+--   implementing 'withLineStyle'.
 withLineStyle' :: ChartBackend m => LineStyle -> m a -> m a
 withLineStyle' ls m = local (\s -> s { cbeLineStyle = ls }) m
 
+-- | Helper to correctly update the backends environment when 
+--   implementing 'withClipRegion'.
 withClipRegion' :: ChartBackend m => Rect -> m a -> m a
 withClipRegion' clip m = local (\s -> s { 
   cbeClipRegion = case cbeClipRegion s of
