c97564aaea46b50f1e75fec2c08be4384ef76da5
Jan Bracker <jan.bracker@googlemail.com>
2013-06-24 11:05:19 +0200

    Moved all Line, Fill and Font types to the Backend module.

--- chart/Graphics/Rendering/Chart/Backend.hs
+++ chart/Graphics/Rendering/Chart/Backend.hs
@@ -1,25 +1,57 @@
 
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 -- | This module provides the interface class for 'ChartBackend's.
 module Graphics.Rendering.Chart.Backend
-  ( ChartBackendEnv(..)
-  , defaultEnv
-  
+  ( -- * Backend Types
+    ChartBackendEnv(..)
   , ChartBackend(..)
   , TextSize(..)
   
+  -- * Backend Helpers
+  , defaultEnv
   , getTransform
   , getFillStyle, getFontStyle
   , getLineStyle, getClipRegion
   , withTransform'
   , withFillStyle', withFontStyle'
   , withLineStyle', withClipRegion'
+  
+  -- * Line Types
+  , LineCap(..)
+  , LineJoin(..)
+  , LineStyle(..)
+  
+  , line_width
+  , line_color
+  , line_dashes
+  , line_cap
+  , line_join
+  
+  -- * Fill Types
+  , FillStyle(..)
+
+  -- * Font and Text Types
+  , FontWeight(..)
+  , FontSlant(..)
+  , FontStyle(..)
+  
+  , HTextAnchor(..)
+  , VTextAnchor(..)
+
+  , font_name
+  , font_size
+  , font_slant
+  , font_weight
+  , font_color
   ) where
 
 import Data.Colour
 import Data.Default
 import Data.Monoid
+import Data.Colour.Names
+import Data.Accessor.Template
 
 import Control.Monad.Reader
 
@@ -147,19 +179,6 @@ class (Monad m, MonadReader ChartBackendEnv m) => ChartBackend m where
   withClipRegion :: Rect -> m a -> m a
 
 -- -----------------------------------------------------------------------
--- Rendering Utility Types
--- -----------------------------------------------------------------------
-
--- | Text metrics returned by 'textSize'.
-data TextSize = TextSize 
-  { textSizeWidth    :: Double -- ^ The total width of the text.
-  , textSizeAscent   :: Double -- ^ The ascent or space above the baseline.
-  , textSizeDescent  :: Double -- ^ The decent or space below the baseline.
-  , textSizeYBearing :: Double -- ^ The Y bearing.
-  , textSizeHeight   :: Double -- ^ The total height of the text.
-  }
-
--- -----------------------------------------------------------------------
 -- Rendering Utility Functions
 -- -----------------------------------------------------------------------
 
@@ -215,3 +234,106 @@ withClipRegion' clip m = local (\s -> s {
                       Nothing -> Just $ Rect (Point 0 0) (Point 0 0)
                       Just intersection -> Just intersection
   }) m
+
+-- -----------------------------------------------------------------------
+-- Line Types
+-- -----------------------------------------------------------------------
+
+-- | The different supported line ends.
+data LineCap = LineCapButt   -- ^ Just cut the line straight.
+             | LineCapRound  -- ^ Make a rounded line end.
+             | LineCapSquare -- ^ Make a square that ends the line.
+
+-- | The different supported ways to join line ends.
+data LineJoin = LineJoinMiter -- ^ Extends the outline until they meet each other.
+              | LineJoinRound -- ^ Draw a circle fragment to connet line end.
+              | LineJoinBevel -- ^ Like miter, but cuts it off if a certain 
+                              --   threshold is exceeded.
+
+-- | Data type for the style of a line.
+data LineStyle = LineStyle {
+   line_width_  :: Double,
+   line_color_  :: AlphaColour Double,
+   line_dashes_ :: [Double],
+   line_cap_    :: LineCap,
+   line_join_   :: LineJoin
+}
+
+instance Default LineStyle where
+  def = LineStyle 
+    { line_width_  = 1
+    , line_color_  = opaque black
+    , line_dashes_ = []
+    , line_cap_    = LineCapButt
+    , line_join_   = LineJoinBevel
+    }
+
+-- -----------------------------------------------------------------------
+-- Font & Text Types
+-- -----------------------------------------------------------------------
+
+-- | The possible slants of a font.
+data FontSlant = FontSlantNormal  -- ^ Normal font style without slant.
+               | FontSlantItalic  -- ^ With a slight slant.
+               | FontSlantOblique -- ^ With a greater slant.
+
+instance Default FontSlant where
+  def = FontSlantNormal
+
+-- | The possible weights of a font.
+data FontWeight = FontWeightNormal -- ^ Normal font style without weight.
+                | FontWeightBold   -- ^ Bold font.
+
+instance Default FontWeight where
+  def = FontWeightNormal
+
+-- | Data type for a font.
+data FontStyle = FontStyle {
+      font_name_   :: String,
+      font_size_   :: Double,
+      font_slant_  :: FontSlant,
+      font_weight_ :: FontWeight,
+      font_color_  :: AlphaColour Double
+}
+
+instance Default FontStyle where
+  def = FontStyle 
+    { font_name_   = "sans"
+    , font_size_   = 10
+    , font_slant_  = def
+    , font_weight_ = def
+    , font_color_  = opaque black
+    }
+
+data HTextAnchor = HTA_Left | HTA_Centre | HTA_Right
+data VTextAnchor = VTA_Top | VTA_Centre | VTA_Bottom | VTA_BaseLine
+
+-- | Text metrics returned by 'textSize'.
+data TextSize = TextSize 
+  { textSizeWidth    :: Double -- ^ The total width of the text.
+  , textSizeAscent   :: Double -- ^ The ascent or space above the baseline.
+  , textSizeDescent  :: Double -- ^ The decent or space below the baseline.
+  , textSizeYBearing :: Double -- ^ The Y bearing.
+  , textSizeHeight   :: Double -- ^ The total height of the text.
+  }
+
+-- -----------------------------------------------------------------------
+-- Fill Types
+-- -----------------------------------------------------------------------
+
+-- | Abstract data type for a fill style.
+--
+--   The contained Cairo action sets the required fill
+--   style in the Cairo rendering state.
+newtype FillStyle = FillStyleSolid { fill_colour_ :: AlphaColour Double }
+
+instance Default FillStyle where
+  def = FillStyleSolid
+    { fill_colour_ = opaque white
+    }
+
+-- -----------------------------------------------------------------------
+-- Template haskell to derive an instance of Data.Accessor.Accessor
+-- for each field.
+$( deriveAccessors '' LineStyle )
+$( deriveAccessors '' FontStyle )
--- chart/Graphics/Rendering/Chart/Types.hs
+++ chart/Graphics/Rendering/Chart/Types.hs
@@ -3,30 +3,7 @@
 {-# OPTIONS_GHC -XTemplateHaskell #-}
 
 module Graphics.Rendering.Chart.Types
-  ( LineCap(..)
-  , LineJoin(..)
-  , LineStyle(..)
-  
-  , FontWeight(..)
-  , FontSlant(..)
-  , FontStyle(..)
-  
-  , FillStyle(..)
-
-  , HTextAnchor(..)
-  , VTextAnchor(..)
-  
-  , line_width
-  , line_color
-  , line_dashes
-  , line_cap
-  , line_join
-
-  , font_name
-  , font_size
-  , font_slant
-  , font_weight
-  , font_color
+  ( 
   ) where
 
 import Data.Colour
@@ -38,97 +15,5 @@ import Data.Monoid
 
 import Graphics.Rendering.Chart.Geometry
 
--- -----------------------------------------------------------------------
--- Line Types
--- -----------------------------------------------------------------------
 
--- | The different supported line ends.
-data LineCap = LineCapButt   -- ^ Just cut the line straight.
-             | LineCapRound  -- ^ Make a rounded line end.
-             | LineCapSquare -- ^ Make a square that ends the line.
-
--- | The different supported ways to join line ends.
-data LineJoin = LineJoinMiter -- ^ Extends the outline until they meet each other.
-              | LineJoinRound -- ^ Draw a circle fragment to connet line end.
-              | LineJoinBevel -- ^ Like miter, but cuts it off if a certain 
-                              --   threshold is exceeded.
-
--- | Data type for the style of a line.
-data LineStyle = LineStyle {
-   line_width_  :: Double,
-   line_color_  :: AlphaColour Double,
-   line_dashes_ :: [Double],
-   line_cap_    :: LineCap,
-   line_join_   :: LineJoin
-}
-
-instance Default LineStyle where
-  def = LineStyle 
-    { line_width_  = 1
-    , line_color_  = opaque black
-    , line_dashes_ = []
-    , line_cap_    = LineCapButt
-    , line_join_   = LineJoinBevel
-    }
-
--- -----------------------------------------------------------------------
--- Font & Text Types
--- -----------------------------------------------------------------------
-
--- | The possible slants of a font.
-data FontSlant = FontSlantNormal  -- ^ Normal font style without slant.
-               | FontSlantItalic  -- ^ With a slight slant.
-               | FontSlantOblique -- ^ With a greater slant.
-
-instance Default FontSlant where
-  def = FontSlantNormal
-
--- | The possible weights of a font.
-data FontWeight = FontWeightNormal -- ^ Normal font style without weight.
-                | FontWeightBold   -- ^ Bold font.
-
-instance Default FontWeight where
-  def = FontWeightNormal
-
--- | Data type for a font.
-data FontStyle = FontStyle {
-      font_name_   :: String,
-      font_size_   :: Double,
-      font_slant_  :: FontSlant,
-      font_weight_ :: FontWeight,
-      font_color_  :: AlphaColour Double
-}
-
-instance Default FontStyle where
-  def = FontStyle 
-    { font_name_   = "sans"
-    , font_size_   = 10
-    , font_slant_  = def
-    , font_weight_ = def
-    , font_color_  = opaque black
-    }
-
-data HTextAnchor = HTA_Left | HTA_Centre | HTA_Right
-data VTextAnchor = VTA_Top | VTA_Centre | VTA_Bottom | VTA_BaseLine
-
--- -----------------------------------------------------------------------
--- Fill Types
--- -----------------------------------------------------------------------
-
--- | Abstract data type for a fill style.
---
---   The contained Cairo action sets the required fill
---   style in the Cairo rendering state.
-newtype FillStyle = FillStyleSolid { fill_colour_ :: AlphaColour Double }
-
-instance Default FillStyle where
-  def = FillStyleSolid
-    { fill_colour_ = opaque white
-    }
-
--- -----------------------------------------------------------------------
--- Template haskell to derive an instance of Data.Accessor.Accessor
--- for each field.
-$( deriveAccessors '' LineStyle )
-$( deriveAccessors '' FontStyle )
 
