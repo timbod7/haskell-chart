d53e19301f71f584a49e2306a34fd4dfece3aa4d
Jan Bracker <jan.bracker@googlemail.com>
2013-07-15 15:24:09 +0200

    First try on implementing fill and stroke operations and applying their styles.

--- chart-diagrams/Graphics/Rendering/Chart/Backend/Diagrams.hs
+++ chart-diagrams/Graphics/Rendering/Chart/Backend/Diagrams.hs
@@ -14,8 +14,10 @@ import Data.Monoid
 
 import Control.Monad.Reader
 
-import Diagrams.Prelude (Diagram, R2)
+import Diagrams.Prelude (Diagram, R2, r2, p2, Trail, (.+^))
 import qualified Diagrams.Prelude as D
+import qualified Diagrams.TwoD as D2
+import qualified Diagrams.TwoD.Arc as D2
 
 import Graphics.Rendering.Chart.Backend as G
 import Graphics.Rendering.Chart.Backend.Utils
@@ -36,13 +38,36 @@ runBackend env m = compileBackend
       withTransformD withLineStyleD withFillStyleD withFontStyleD withClipRegionD
       env m
 
+
+{-
+data Path = MoveTo Point Path 
+          | LineTo Point Path
+          | Arc Point Double Double Double Path
+          | ArcNeg Point Double Double Double Path
+          | End 
+          | Close
+ -}
+
+{-
+
+ foldPath :: (Monoid m)
+         => (Point -> m) -- ^ MoveTo
+         -> (Point -> m) -- ^ LineTo
+         -> (Point -> Double -> Double -> Double -> m) -- ^ Arc
+         -> (Point -> Double -> Double -> Double -> m) -- ^ ArcNeg
+         -> m    -- ^ Close
+         -> Path -- ^ Path to fold
+         -> m
+ 
+ -}
+
 strokePathD :: (D.Renderable (D.Path R2) b)
             => ChartBackendEnv -> Path -> Diagram b R2
-strokePathD = undefined
+strokePathD env p = applyLineStyle (cbeLineStyle env) $ D.stroke $ convertPath p
 
 fillPathD :: (D.Renderable (D.Path R2) b)
           => ChartBackendEnv -> Path -> Diagram b R2
-fillPathD = undefined
+fillPathD env p = applyFillStyle (cbeFillStyle env) $ D.stroke $ convertPath p
 
 fillClipD :: (D.Renderable (D.Path R2) b)
           => ChartBackendEnv -> Diagram b R2
@@ -62,11 +87,11 @@ withTransformD = undefined
 
 withLineStyleD :: (D.Renderable (D.Path R2) b)
                => ChartBackendEnv -> Diagram b R2 -> Diagram b R2
-withLineStyleD = undefined
+withLineStyleD env = applyLineStyle (cbeLineStyle env)
 
 withFillStyleD :: (D.Renderable (D.Path R2) b)
                => ChartBackendEnv -> Diagram b R2 -> Diagram b R2
-withFillStyleD = undefined
+withFillStyleD env = applyFillStyle (cbeFillStyle env)
 
 withFontStyleD :: (D.Renderable (D.Path R2) b)
                => ChartBackendEnv -> Diagram b R2 -> Diagram b R2
@@ -76,10 +101,70 @@ withClipRegionD :: (D.Renderable (D.Path R2) b)
                 => ChartBackendEnv -> Diagram b R2 -> Diagram b R2
 withClipRegionD = undefined
 
+-- -----------------------------------------------------------------------
+-- Converions Helpers
+-- -----------------------------------------------------------------------
 
+-- | Apply the Chart line style to a diagram.
+applyLineStyle :: (D.HasStyle a) => LineStyle -> a -> a
+applyLineStyle ls = D.lineWidth (line_width_ ls) 
+                  . D.lineColor (line_color_ ls) 
+                  . D.lineCap (convertLineCap $ line_cap_ ls) 
+                  . D.lineJoin (convertLineJoin $ line_join_ ls) 
+                  . D.dashing (line_dashes_ ls) 0
 
+-- | Apply the Chart fill style to a diagram.
+applyFillStyle :: (D.HasStyle a) => FillStyle -> a -> a
+applyFillStyle fs = case fs of
+  FillStyleSolid cl -> D.fillColor cl
 
+-- | Convert line caps.
+convertLineCap :: LineCap -> D.LineCap
+convertLineCap cap = case cap of
+  LineCapButt   -> D.LineCapButt
+  LineCapRound  -> D.LineCapRound
+  LineCapSquare -> D.LineCapSquare
 
+-- | Convert line joins.
+convertLineJoin :: LineJoin -> D.LineJoin
+convertLineJoin join = case join of
+  LineJoinMiter -> D.LineJoinMiter
+  LineJoinRound -> D.LineJoinRound
+  LineJoinBevel -> D.LineJoinBevel
+
+-- | Convert paths.
+convertPath :: Path -> D.Path R2
+convertPath p = convertPath' (p2 (0,0)) p
+  where
+    convertPath' :: D.Point R2 -> Path -> D.Path R2
+    convertPath' offset p = 
+      let (start, t, restP) = pathToTrail offset p
+      in D.pathFromTrailAt t start <> case restP of
+        Nothing -> mempty
+        Just rest -> convertPath' (start .+^ D.trailOffset t) rest
+
+pathToTrail :: D.Point R2 -> Path 
+            -> (D.Point R2, Trail R2, Maybe Path)
+pathToTrail start (MoveTo (Point x y) p) = 
+  let (t, rest) = pathToTrail' p
+  in (p2 (x,y), t, rest)
+pathToTrail start p = 
+  let (t, rest) = pathToTrail' p
+  in (start, t, rest)
+
+pathToTrail' :: Path -> (Trail R2, Maybe Path)
+pathToTrail' p@(MoveTo _ _) = (mempty, Just p)
+pathToTrail' (LineTo (Point x y) p) = 
+  let (t, rest) = pathToTrail' p
+  in (D.fromSegments [D.straight $ r2 (x,y)] <> t, rest)
+pathToTrail' (Arc (Point x y) r as ae p) = 
+  let (t, rest) = pathToTrail' p
+  in ((D2.scale r $ D2.arc (D2.Rad as) (D2.Rad ae)) <> t, rest)
+pathToTrail' (ArcNeg (Point x y) r as ae p) = 
+  let (t, rest) = pathToTrail' p
+  in ((D2.scale r $ D2.arcCW (D2.Rad as) (D2.Rad ae)) <> t, rest)
+pathToTrail' End = (mempty, Nothing)
+pathToTrail' Close = (D.close mempty, Nothing)
 
 
 
