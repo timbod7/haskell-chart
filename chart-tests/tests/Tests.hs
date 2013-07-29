c726c02288919dd56a9e73fdf8c6ac72b207fa48
Jan Bracker <jan.bracker@googlemail.com>
2013-07-29 14:13:36 +0200

    Moved the cairo specific test stuff to separate file.

--- chart-tests/tests/Tests.hs
+++ chart-tests/tests/Tests.hs
@@ -1,10 +1,11 @@
+
+module Tests where
+
 import Graphics.Rendering.Chart
 import Graphics.Rendering.Chart.Drawing
 import Graphics.Rendering.Chart.Simple
 import Graphics.Rendering.Chart.Grid
-import Graphics.Rendering.Chart.Backend.Cairo
 
-import System.Environment(getArgs)
 import System.Time
 import System.Random
 import Data.Time.LocalTime
@@ -407,16 +408,7 @@ allTests =
   where simple :: Renderable a -> Renderable ()
         simple = mapPickFn (const ())
 
-main = do
-    args <- getArgs
-    main1 args
 
-main1 :: [String] -> IO ()
-main1 ("--pdf":tests) = showTests tests renderToPDF
-main1 ("--svg":tests) = showTests tests renderToSVG
-main1 ("--ps":tests) = showTests tests renderToPS
-main1 ("--png":tests) = showTests tests renderToPNG
-main1 tests = showTests tests renderToPNG
 
 showTests :: [String] -> ((String,(Int,Int),OutputType -> Renderable ()) -> IO()) -> IO ()
 showTests tests ofn = mapM_ doTest (filter (match tests) allTests)
@@ -429,9 +421,3 @@ showTests tests ofn = mapM_ doTest (filter (match tests) allTests)
 match :: [String] -> (String,s,a) -> Bool
 match [] t = True
 match ts (s,_,_) = s `elem` ts
-
-renderToPNG (n,(w,h),ir) = renderableToPNGFile (ir PNG) w h (n ++ ".png")
-                           >> return ()
-renderToPS  (n,(w,h),ir) = renderableToPSFile (ir PS) w h (n ++ ".ps")
-renderToPDF (n,(w,h),ir) = renderableToPDFFile (ir PDF) w h (n ++ ".pdf")
-renderToSVG (n,(w,h),ir) = renderableToSVGFile (ir SVG) w h (n ++ ".svg")
