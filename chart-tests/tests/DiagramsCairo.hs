e01257cda11d9832ac9a3c7af424c02bd83cb1a2
Jan Bracker <jan.bracker@googlemail.com>
2013-07-29 17:16:48 +0200

    Updated tests with the changes in the diagrams backend. Environment have to be created using IO.

--- chart-tests/tests/DiagramsCairo.hs
+++ chart-tests/tests/DiagramsCairo.hs
@@ -6,7 +6,7 @@ import Diagrams.Core.Types ( renderDia )
 import Diagrams.TwoD ( SizeSpec2D(..) )
 import Diagrams.Backend.Cairo
 import Diagrams.Backend.Cairo.Internal
-import Graphics.Rendering.Chart.Renderable ( render )
+import Graphics.Rendering.Chart.Renderable ( render, Renderable )
 
 import System.Environment ( getArgs )
 
@@ -18,9 +18,13 @@ main = do
     main1 args
 
 main1 :: [String] -> IO ()
-main1 args = showTests (fmap (\(x,_,_) -> x) allTests) renderDiagram
-
-renderDiagram (n,(w,h),ir) = do
-  let cr = render (ir T.PNG) (fromIntegral w, fromIntegral h)
-  let (d, _) = runBackend (defaultEnv bitmapAlignmentFns) cr
-  fst $ renderDia Cairo (CairoOptions (n ++ ".png") (Dims (fromIntegral w) (fromIntegral h)) PNG True) d
+main1 args = do
+  -- Only load the environment once, to speed things up.
+  env <- defaultEnv bitmapAlignmentFns
+  let renderDiagram :: (String, (Int, Int), T.OutputType -> Renderable ()) -> IO ()
+      renderDiagram (n,(w,h),ir) = do
+        let cr = render (ir T.PNG) (fromIntegral w, fromIntegral h)
+        let (d, _) = runBackend env cr
+        fst $ renderDia Cairo (CairoOptions (n ++ ".png") (Dims (fromIntegral w) (fromIntegral h)) PNG True) d
+  showTests (fmap (\(x,_,_) -> x) allTests) renderDiagram
+  
