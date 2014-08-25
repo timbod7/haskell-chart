ce4b8c3570901b62fe30f2873a9ea1ded36c5e8b
Tim Docker <tim@dockerz.net>
2014-08-25 15:48:49 +1000

    Use nested state to simplify construction syntax

--- chart/Graphics/Rendering/Chart/State.hs
+++ chart/Graphics/Rendering/Chart/State.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell, FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables, TemplateHaskell, FlexibleInstances #-}
 module Graphics.Rendering.Chart.State(
   plot,
   plotLeft,
@@ -9,14 +9,14 @@ module Graphics.Rendering.Chart.State(
   takeShape,
 
   CState,
-  layout,
   colors,
-  shapes
-  
+  shapes,
+
+  execEC
   ) where
 
 import Control.Lens
-import Control.Monad.State(State,execState)
+import Control.Monad.State
 import Data.Default.Class
 import Data.List(cycle)
 
@@ -31,53 +31,62 @@ import Graphics.Rendering.Chart.Drawing
 import Graphics.Rendering.Chart.Renderable
 
 -- | The state held when monadically constructing a graphical element
-data CState l = CState {
-  _layout :: l,                    -- ^ The graphical element being constructed
+data CState = CState {
   _colors :: [AlphaColour Double], -- ^ An infinite source of colors, for use in plots
   _shapes :: [PointShape]          -- ^ An infinite source of shapes, for use in plots
   }
 
 $( makeLenses ''CState )
 
-type EC l a = State (CState l) a
+-- | We use nested State monads to give nice syntax. The outer state
+-- is the graphical element being constructed (typically a
+-- layout). The inner state contains any additional state
+-- reqired. This approach means that lenses and the state monad lens
+-- operators can be used directly on the value being constructed.
+type EC l a = StateT l (State CState) a
 
-instance (Default l) => Default (CState l) where
-  def = CState def colors shapes
+instance Default CState where
+  def = CState colors shapes
     where
       colors = cycle (map opaque [blue,red,green,orange,yellow,violet])
       shapes = cycle [PointShapeCircle,PointShapePlus,PointShapeCross,PointShapeStar]
       
 instance (Default l,ToRenderable l) => ToRenderable (EC l a) where
-  toRenderable ec = toRenderable (_layout (execState ec def))
+  toRenderable ec = toRenderable (execEC ec)
+
+-- | Run the monadic `EC` computation, and return the graphical
+-- element (ie the outer monad' state)
+execEC :: forall a l . (Default l) => EC l a -> l
+execEC ec = evalState (execStateT ec def) def
 
 -- | Add a plot to the `Layout` being constructed.
 plot :: ToPlot p => EC (Layout x y) (p x y) -> EC (Layout x y) ()
 plot pm = do
   p <- pm
-  layout . layout_plots %= (toPlot p:)
+  layout_plots %= (toPlot p:)
 
 -- | Add a plot against the left axis to the `LayoutLR` being constructed.
 plotLeft :: ToPlot p => EC (LayoutLR x y1 y2 ) (p x y1) -> EC (LayoutLR x y1 y2) ()
 plotLeft pm = do
   p <- pm
-  layout . layoutlr_plots %= (Left (toPlot p):)
+  layoutlr_plots %= (Left (toPlot p):)
 
 -- | Add a plot against the right axis tof the `LayoutLR` being constructed.
 plotRight :: ToPlot p => EC (LayoutLR x y1 y2 ) (p x y2) -> EC (LayoutLR x y1 y2) ()
 plotRight pm = do
   p <- pm
-  layout . layoutlr_plots %= (Right (toPlot p):)
+  layoutlr_plots %= (Right (toPlot p):)
 
 -- | Pop and return the next color from the state
 takeColor :: EC l (AlphaColour Double)
-takeColor = do
+takeColor = lift $ do
   (c:cs) <- use colors
   colors .= cs
   return c
 
 -- | Pop and return the next shape from the state
 takeShape :: EC l PointShape
-takeShape = do
+takeShape = lift $ do
   (c:cs) <- use shapes
   shapes .= cs
   return c
