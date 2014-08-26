66c8d50112ac49dcd73d2fc9c0283d2a6288614c
Tim Docker <tim@dockerz.net>
2014-08-26 18:52:24 +1000

    Can now nest ECs and hence use them for elements of layouts

--- chart/Graphics/Rendering/Chart/State.hs
+++ chart/Graphics/Rendering/Chart/State.hs
@@ -13,6 +13,7 @@ module Graphics.Rendering.Chart.State(
   shapes,
 
   execEC,
+  embedEC,
   liftCState,
   ) where
 
@@ -60,26 +61,35 @@ instance (Default l,ToRenderable l) => ToRenderable (EC l a) where
 execEC :: (Default l) => EC l a -> l
 execEC ec = evalState (execStateT ec def) def
 
+-- | Nest the construction of a graphical element within
+-- the construction of another.
+embedEC :: (Default l1) => EC l1 a -> EC l2 l1
+embedEC ec = do
+  cs <- lift get
+  let (l,cs') = runState (execStateT ec def) cs
+  lift (put cs')
+  return l
+
 -- | Lift a a computation over `CState`
 liftCState :: State CState a -> EC l a
 liftCState = lift
 
 -- | Add a plot to the `Layout` being constructed.
-plot :: ToPlot p => EC (Layout x y) (p x y) -> EC (Layout x y) ()
+plot :: (Default (p x y), ToPlot p) => EC (p x y) a -> EC (Layout x y) ()
 plot pm = do
-  p <- pm
-  layout_plots %= (toPlot p:)
+    p <- embedEC pm
+    layout_plots %= (toPlot p:)
 
 -- | Add a plot against the left axis to the `LayoutLR` being constructed.
-plotLeft :: ToPlot p => EC (LayoutLR x y1 y2 ) (p x y1) -> EC (LayoutLR x y1 y2) ()
+plotLeft :: (Default (p x y1), ToPlot p) => EC (p x y1) a -> EC (LayoutLR x y1 y2) ()
 plotLeft pm = do
-  p <- pm
+  p <- embedEC pm
   layoutlr_plots %= (Left (toPlot p):)
 
 -- | Add a plot against the right axis tof the `LayoutLR` being constructed.
-plotRight :: ToPlot p => EC (LayoutLR x y1 y2 ) (p x y2) -> EC (LayoutLR x y1 y2) ()
+plotRight :: (Default (p x y2), ToPlot p) => EC (p x y2) a -> EC (LayoutLR x y1 y2) ()
 plotRight pm = do
-  p <- pm
+  p <- embedEC pm
   layoutlr_plots %= (Right (toPlot p):)
 
 -- | Pop and return the next color from the state
@@ -98,26 +108,22 @@ takeShape = liftCState $ do
 
 -- | Constuct a line plot with the given title and
 -- data, using the next available color.
-line :: String -> [[(x,y)]]  -> EC l (PlotLines x y)
+line :: String -> [[(x,y)]]  -> EC (PlotLines x y) ()
 line title values = do
     color <- takeColor
-    return (
-      plot_lines_values .~ values $
-      plot_lines_title .~ title $
-      plot_lines_style . line_color .~ color $
-      def )
+    plot_lines_title .= title
+    plot_lines_values .= values
+    plot_lines_style . line_color .= color
 
--- | Construct a scatter plot with the given title and dtda, using the
+-- | Construct a scatter plot with the given title and data, using the
 -- next available color and point shape.
-points :: String -> [(x,y)]  -> EC l (PlotPoints x y)
+points :: String -> [(x,y)]  -> EC (PlotPoints x y) ()
 points title values = do
     color <- takeColor
     shape <- takeShape
-    return (
-      plot_points_values .~ values $
-      plot_points_title .~ title $
-      plot_points_style . point_color .~ color $
-      plot_points_style . point_shape .~ shape $
-      plot_points_style . point_radius .~ 2 $
-      def )
+    plot_points_values .= values
+    plot_points_title .= title
+    plot_points_style . point_color .= color
+    plot_points_style . point_shape .= shape
+    plot_points_style . point_radius .= 2
 
