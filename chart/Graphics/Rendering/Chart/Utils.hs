3b124386fdbdae7af1cb328bb340c7df801ce4b5
Sidharth Kapur <sidharthkapur1@gmail.com>
2016-03-14 23:44:30 -0500

    Undo accidental changes to Utils.hs

--- chart/Graphics/Rendering/Chart/Utils.hs
+++ chart/Graphics/Rendering/Chart/Utils.hs
@@ -4,7 +4,7 @@ module Graphics.Rendering.Chart.Utils(
     maybeM,
   ) where
 
--- | Checks if the given value is and actual numeric value and not
+-- | Checks if the given value is and actual numeric value and not 
 --   a concept like NaN or infinity.
 isValidNumber :: (RealFloat a) => a -> Bool
 isValidNumber v = not (isNaN v) && not (isInfinite v)
@@ -12,3 +12,5 @@ isValidNumber v = not (isNaN v) && not (isInfinite v)
 -- | Version of 'Prelude.maybe' that returns a monadic value.
 maybeM :: (Monad m) => b -> (a -> m b) -> Maybe a -> m b
 maybeM v = maybe (return v)
+
+
