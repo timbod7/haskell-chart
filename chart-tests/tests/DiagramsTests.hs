0dba1cf212681c30ecc060474256833338e2605c
Sidharth Kapur <sidharthkapur1@gmail.com>
2016-03-15 12:04:47 -0500

    Small changes

--- chart-tests/tests/DiagramsTests.hs
+++ chart-tests/tests/DiagramsTests.hs
@@ -6,7 +6,7 @@ import Graphics.Rendering.Chart.Backend.Diagrams
 import Control.Monad (void)
 
 import Diagrams.Core (renderDia)
-import Diagrams.Prelude ( dims, V2(..) )
+import Diagrams.Prelude (dims, V2(..))
 import Diagrams.Backend.Cairo
 import Diagrams.Backend.Cairo.Internal
 import Graphics.Rendering.Chart.Renderable (render, Renderable)
@@ -26,28 +26,28 @@ run backend = do
   showTests (fmap (\(x,_,_) -> x) allTests) (go backend env)
 
   where
-    go backend env (name, (w, h), ir) = do
+    go backend env (name, (w,h), ir) = do
       let env' = env { envOutputSize = (fromIntegral w, fromIntegral h) }
       putStrLn (name ++ "...")
       renderDiagram backend env' (name, (fromIntegral w, fromIntegral h), ir)
 
 renderDiagram :: DiagramsBackend -> DEnv Double -> (String, (Double, Double), T.LineWidth -> Renderable ()) -> IO ()
-renderDiagram BackendCairo env (name,(w,h),ir) = do
+renderDiagram BackendCairo env (name, (w,h), ir) = do
   let (diagram, _) = runBackendR env (ir 1.0)
   fst $ renderDia Cairo (CairoOptions (name ++ ".png") (dims $ V2 w h) PNG False) diagram
 
-renderDiagram BackendEPS env (name,(w,h),ir) = do
+renderDiagram BackendEPS env (name, (w,h), ir) = do
   let cr = render (ir 0.25) (w, h)
       path = name ++ ".eps"
   void (cBackendToEPSFile cr env path)
 
-renderDiagram BackendSVG env (name,(w,h),ir) = do
+renderDiagram BackendSVG env (name, (w,h), ir) = do
   let cr = render (ir 0.25) (w, h)
       (svg, _) = cBackendToSVG cr env
       path = name ++ ".svg"
   BS.writeFile path (renderBS svg)
 
-renderDiagram BackendSVGEmbedded env (name,(w,h),ir) = do
+renderDiagram BackendSVGEmbedded env (name, (w,h), ir) = do
   let cr = render (ir 0.25) (w, h)
       (svg, _) = cBackendToEmbeddedFontSVG cr env
       path = name ++ "-embedded.svg"
