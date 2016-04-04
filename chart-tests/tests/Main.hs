28bd6ac3cd9940334d280000e307e936c3c0f059
Sidharth Kapur <sidharthkapur1@gmail.com>
2016-04-04 15:21:52 -0500

    Revert change in tests Main.hs

--- chart-tests/tests/Main.hs
+++ chart-tests/tests/Main.hs
@@ -32,8 +32,8 @@ main1 ("charts-cairo":"--ps":tests) = renderTestsToFiles PS ".ps" tests
 main1 ("charts-cairo":"--png":tests) = renderTestsToFiles PNG ".png" tests
 
 main1 ("charts-diagrams":"--cairo":args) = DiagramsCairo.main1 args
--- main1 ("charts-diagrams":"--svg":args) = DiagramsSVG.main1 args
--- main1 ("charts-diagrams":"--eps":args) = DiagramsEPS.main1 args
+main1 ("charts-diagrams":"--svg":args) = DiagramsSVG.main1 args
+main1 ("charts-diagrams":"--eps":args) = DiagramsEPS.main1 args
 
 main1 ("drawing-cairo":[]) = Drawing.Cairo.main
 main1 ("drawing-diagrams":[]) = Drawing.DiagramsCairo.main
