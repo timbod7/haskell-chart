aad7de65541c16316e0fe9bc1f702a90fc888133
Reuben D'Netto <rdnetto@gmail.com>
2017-05-17 19:08:56 +1000

    Added plot_annotation_offset

--- chart/Graphics/Rendering/Chart/Plot/Annotation.hs
+++ chart/Graphics/Rendering/Chart/Plot/Annotation.hs
@@ -15,14 +15,15 @@ module Graphics.Rendering.Chart.Plot.Annotation(
     plot_annotation_vanchor,
     plot_annotation_angle,
     plot_annotation_style,
-    plot_annotation_values
+    plot_annotation_values,
+    plot_annotation_offset
 ) where
 
 import Control.Lens
-import Graphics.Rendering.Chart.Geometry
-import Graphics.Rendering.Chart.Drawing
-import Graphics.Rendering.Chart.Plot.Types
 import Data.Default.Class
+import Graphics.Rendering.Chart.Drawing
+import Graphics.Rendering.Chart.Geometry
+import Graphics.Rendering.Chart.Plot.Types
 
 -- | Value for describing a series of text annotations
 --   to be placed at arbitrary points on the graph. Annotations
@@ -34,7 +35,8 @@ data PlotAnnotation  x y = PlotAnnotation {
       _plot_annotation_angle   :: Double,
       -- ^ Angle, in degrees, to rotate the annotation about the anchor point.
       _plot_annotation_style   :: FontStyle,
-      _plot_annotation_values  :: [(x,y,String)]
+      _plot_annotation_values  :: [(x,y,String)],
+      _plot_annotation_offset  :: Vector
 }
 
 
@@ -56,8 +58,9 @@ renderAnnotation p pMap = withFontStyle style $
           values = _plot_annotation_values p
           angle =  _plot_annotation_angle p
           style =  _plot_annotation_style p
+          offset = _plot_annotation_offset p
           drawOne (x,y,s) = drawTextsR hta vta angle point s
-              where point = pMap (LValue x, LValue y)
+              where point = pMap (LValue x, LValue y) `pvadd` offset
 
 instance Default (PlotAnnotation x y) where
   def = PlotAnnotation 
@@ -66,6 +69,7 @@ instance Default (PlotAnnotation x y) where
     , _plot_annotation_angle   = 0
     , _plot_annotation_style   = def
     , _plot_annotation_values  = []
+    , _plot_annotation_offset  = Vector 0 0
     }
 
 $( makeLenses ''PlotAnnotation )
